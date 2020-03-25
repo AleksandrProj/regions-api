a01b2336f05917157ab094027b257ac72f4251c8
Aleksandr <finast@mail.ru>
2020-03-25 21:36:12 +0400

    git

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'ProstKarPost7890'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class DevelopmentConfig(Config):
     DEBUG = True
